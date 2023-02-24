9ef8c1335e02c6b6194b35346c54810e061d2525
Gerber, Mike <mike.gerber@sbb.spk-berlin.de>
2023-02-24 19:03:42 +0100

    ✔ Add a basic test

---
+++ Makefile
@@ -0,0 +1,85 @@
+export  # export variables to subshells
+PIP_INSTALL = pip3 install
+GIT_CLONE = git clone
+PYTHON = python3
+PYTEST_ARGS = -W 'ignore::DeprecationWarning' -W 'ignore::FutureWarning'
+TROCR_MODEL = microsoft/trocr-small-printed
+
+# BEGIN-EVAL makefile-parser --make-help Makefile
+
+help:
+	@echo ""
+	@echo "  Targets"
+	@echo ""
+	@echo "    install          Install ocrd_calamari"
+	@echo "    $(MODEL)         Get Calamari model (from SBB)"
+	@echo "    actevedef_718448162 Download example data"
+	@echo "    deps-test        Install testing python deps via pip"
+	@echo "    repo/assets      Clone OCR-D/assets to ./repo/assets"
+	@echo "    test/assets      Setup test assets"
+	@echo "    assets-clean     Remove symlinks in test/assets"
+	@echo "    test             Run unit tests"
+	@echo "    coverage         Run unit tests and determine test coverage"
+	@echo ""
+	@echo "  Variables"
+	@echo ""
+	@echo "    PYTHON       '$(PYTHON)'"
+	@echo "    PIP_INSTALL  '$(PIP_INSTALL)'"
+	@echo "    GIT_CLONE    '$(GIT_CLONE)'"
+	@echo "    MODEL        '$(MODEL)'"
+
+# END-EVAL
+
+# Install
+install:
+	$(PIP_INSTALL) .
+
+
+# Download example data (not used currently)
+actevedef_718448162:
+	wget https://qurator-data.de/examples/actevedef_718448162.zip \
+	&& unzip actevedef_718448162.zip \
+	&& rm actevedef_718448162.zip
+
+
+
+#
+# Assets and Tests
+#
+
+# Install testing python deps via pip
+deps-test:
+	$(PIP_INSTALL) -r requirements-test.txt
+
+deps-test-ubuntu: deps-test
+	apt-get install -y make git curl wget imagemagick
+
+
+# Clone OCR-D/assets to ./repo/assets
+repo/assets:
+	mkdir -p $(dir $@)
+	git clone https://github.com/OCR-D/assets "$@"
+
+
+# Setup test assets
+test/assets: repo/assets
+	mkdir -p $@
+	cp -r -t $@ repo/assets/data/*
+
+# Remove symlinks in test/assets
+assets-clean:
+	rm -rf test/assets
+
+# Run unit tests
+test: test/assets $(MODEL)
+	# declare -p HTTP_PROXY
+	$(PYTHON) -m pytest --continue-on-collection-errors test $(PYTEST_ARGS)
+
+# Run unit tests and determine test coverage
+coverage: test/assets $(MODEL)
+	coverage erase
+	make test PYTHON="coverage run"
+	coverage report
+	coverage html
+
+.PHONY: install assets-clean deps-test test coverage $(MODEL)
---
+++ test/test_recognize.py
@@ -0,0 +1,107 @@
+import os
+import shutil
+import subprocess
+import tempfile
+import urllib.request
+from lxml import etree
+from glob import glob
+
+import pytest
+import logging
+from ocrd.resolver import Resolver
+
+from ocrd_trocr import TrOcrRecognize
+from .base import assets
+
+METS_KANT = assets.url_of('kant_aufklaerung_1784-page-region-line-word_glyph/data/mets.xml')
+WORKSPACE_DIR = tempfile.mkdtemp(prefix='test-ocrd-trocr-')
+TROCR_MODEL = os.getenv('TROCR_MODEL')
+
+
+def page_namespace(tree):
+    """Return the PAGE content namespace used in the given ElementTree.
+
+    This relies on the assumption that, in any given PAGE content file, the root element has the local name "PcGts". We
+    do not check if the files uses any valid PAGE namespace.
+    """
+    root_name = etree.QName(tree.getroot().tag)
+    if root_name.localname == "PcGts":
+        return root_name.namespace
+    else:
+        raise ValueError("Not a PAGE tree")
+
+def assertFileContains(fn, text):
+    """Assert that the given file contains a given string."""
+    with open(fn, "r", encoding="utf-8") as f:
+        assert text in f.read()
+
+def assertFileDoesNotContain(fn, text):
+    """Assert that the given file does not contain given string."""
+    with open(fn, "r", encoding="utf-8") as f:
+        assert not text in f.read()
+
+
+@pytest.fixture
+def workspace():
+    if os.path.exists(WORKSPACE_DIR):
+        shutil.rmtree(WORKSPACE_DIR)
+    os.makedirs(WORKSPACE_DIR)
+
+    resolver = Resolver()
+    # due to core#809 this does not always work:
+    #workspace = resolver.workspace_from_url(METS_KANT, dst_dir=WORKSPACE_DIR)
+    # workaround:
+    shutil.rmtree(WORKSPACE_DIR)
+    shutil.copytree(os.path.dirname(METS_KANT), WORKSPACE_DIR)
+    workspace = resolver.workspace_from_url(os.path.join(WORKSPACE_DIR, 'mets.xml'))
+
+    # The binarization options I have are:
+    #
+    # a. ocrd_kraken which tries to install cltsm, whose installation is borken on my machine (protobuf)
+    # b. ocrd_olena which 1. I cannot fully install via pip and 2. whose dependency olena doesn't compile on my
+    #    machine
+    # c. just fumble with the original files
+    #
+    # So I'm going for option c.
+    for imgf in workspace.mets.find_files(fileGrp="OCR-D-IMG"):
+        imgf = workspace.download_file(imgf)
+        path = os.path.join(workspace.directory, imgf.local_filename)
+        subprocess.call(['mogrify', '-threshold', '50%', path])
+
+    # Remove GT Words and TextEquivs, to not accidently check GT text instead of the OCR text
+    # XXX Review data again
+    for of in workspace.mets.find_files(fileGrp="OCR-D-GT-SEG-WORD-GLYPH"):
+        workspace.download_file(of)
+        path = os.path.join(workspace.directory, of.local_filename)
+        tree = etree.parse(path)
+        nsmap_gt = { "pc": page_namespace(tree) }
+        for to_remove in ["//pc:Word", "//pc:TextEquiv"]:
+            for e in tree.xpath(to_remove, namespaces=nsmap_gt):
+                e.getparent().remove(e)
+        tree.write(path, xml_declaration=True, encoding="utf-8")
+        assertFileDoesNotContain(path, "TextEquiv")
+
+    yield workspace
+
+    #shutil.rmtree(WORKSPACE_DIR)
+
+
+def test_recognize(workspace):
+    TrOcrRecognize(
+        workspace,
+        input_file_grp="OCR-D-GT-SEG-WORD-GLYPH",
+        output_file_grp="OCR-D-OCR-TROCR",
+        parameter={
+            "model": TROCR_MODEL,
+        }
+    ).process()
+    workspace.save_mets()
+
+    #page1 = os.path.join(workspace.directory, "OCR-D-OCR-TROCR/OCR-D-OCR-TROCR_0001.xml")
+    page1 = os.path.join(workspace.directory, "OCR-D-OCR-TROCR/OCR-D-OCR-TROCR_phys_0001.xml")
+    assert os.path.exists(page1)
+    assertFileContains(page1, "TextEquiv")
+    # FIXME assertFileContains(page1, "verſchuldeten")
+
+
+# vim:tw=120:
